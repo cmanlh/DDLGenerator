6346bd307890bb68f01edaefb0cd0794a8bdc7fe
tweenkin <kingsen829@hotmail.com>
2017-12-11 16:04:50 +0800

    表加入别名，生成Mapper文件，使用别名代替Pre_表名，解决addDBFields = false，也生成DBFields对应的ResultMap,以及sql

--- src/main/java/com/lifeonwalden/codeGenerator/mybatis/impl/BaseColumnListWithPrefixElementGenerator.java
+++ src/main/java/com/lifeonwalden/codeGenerator/mybatis/impl/BaseColumnListWithPrefixElementGenerator.java
@@ -16,10 +16,16 @@ public class BaseColumnListWithPrefixElementGenerator implements TableElementGen
         XmlElement element = new XmlElement(XMLTag.SQL.getName());
 
         element.addAttribute(new Attribute(XMLAttribute.ID.getName(), "baseColumnListWithPrefix"));
-
+        String alias = table.getAlias();
         StringBuilder sb = new StringBuilder();
+
         for (Column column : table.getColumns()) {
-            sb.append("pre_").append(table.getName().toLowerCase()).append(".").append(column.getName()).append(",");
+            if (alias != null && alias.length() >= 1) {
+                sb.append(alias).append(".").append(column.getName()).append(",");
+            }
+            else {
+                sb.append("pre_").append(table.getName().toLowerCase()).append(".").append(column.getName()).append(",");
+            }
         }
 
         if (sb.length() > 0) {
