2986a8a5d04f848e4e360436da9b0f38f2e0d8bb
LuHong <cmanlh@163.com>
2017-12-12 10:39:01 +0800

    reconstruct the package

---
+++ src/main/java/com/lifeonwalden/codeGenerator/mybatis/impl/select/SQLSelectElementGenerator.java
@@ -0,0 +1,35 @@
+package com.lifeonwalden.codeGenerator.mybatis.impl;
+
+import com.lifeonwalden.codeGenerator.bean.Table;
+import com.lifeonwalden.codeGenerator.bean.config.Config;
+import com.lifeonwalden.codeGenerator.javaClass.impl.DAOGeneratorImpl;
+import com.lifeonwalden.codeGenerator.mybatis.TableElementGenerator;
+import com.lifeonwalden.codeGenerator.mybatis.constant.XMLAttribute;
+import com.lifeonwalden.codeGenerator.mybatis.constant.XMLTag;
+import org.mybatis.generator.dom.xml.Attribute;
+import org.mybatis.generator.dom.xml.TextElement;
+import org.mybatis.generator.dom.xml.XmlElement;
+
+public class SQLSelectElementGenerator implements TableElementGenerator {
+
+    public XmlElement getElement(Table table, Config config) {
+        XmlElement element = new XmlElement(XMLTag.SQL.getName());
+        element.addAttribute(new Attribute(XMLAttribute.ID.getName(), "querySQL"));
+
+        TextElement selectElement = new TextElement("SELECT");
+        element.addElement(selectElement);
+
+        XmlElement includeElement = new XmlElement(XMLTag.INCLUDE.getName());
+        String namespace = config.getDaoInfo().getPackageName() + "." + DAOGeneratorImpl.getDaoName(table, config);
+        includeElement.addAttribute(new Attribute(XMLAttribute.REF_ID.getName(), namespace + ".baseColumnList"));
+        element.addElement(includeElement);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("FROM ").append(table.getName());
+
+        TextElement fromElement = new TextElement(sb.toString());
+        element.addElement(fromElement);
+
+        return element;
+    }
+}
---
+++ src/main/java/com/lifeonwalden/codeGenerator/mybatis/impl/update/SQLDynamicUpdateElementGenerator.java
@@ -0,0 +1,56 @@
+package com.lifeonwalden.codeGenerator.mybatis.impl;
+
+import com.lifeonwalden.codeGenerator.bean.Column;
+import com.lifeonwalden.codeGenerator.bean.Table;
+import com.lifeonwalden.codeGenerator.bean.config.Config;
+import com.lifeonwalden.codeGenerator.constant.ColumnConstraintEnum;
+import com.lifeonwalden.codeGenerator.constant.JdbcTypeEnum;
+import com.lifeonwalden.codeGenerator.mybatis.TableElementGenerator;
+import com.lifeonwalden.codeGenerator.mybatis.constant.XMLAttribute;
+import com.lifeonwalden.codeGenerator.mybatis.constant.XMLTag;
+import com.lifeonwalden.codeGenerator.util.StringUtil;
+import org.mybatis.generator.dom.xml.Attribute;
+import org.mybatis.generator.dom.xml.TextElement;
+import org.mybatis.generator.dom.xml.XmlElement;
+
+public class SQLDynamicUpdateElementGenerator implements TableElementGenerator {
+
+    public XmlElement getElement(Table table, Config config) {
+        XmlElement element = new XmlElement(XMLTag.SQL.getName());
+
+        element.addAttribute(new Attribute(XMLAttribute.ID.getName(), "dynamicUpdateSQL"));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("update ").append(table.getName());
+
+        element.addElement(new TextElement(sb.toString()));
+
+        XmlElement trimElement = new XmlElement(XMLTag.TRIM.getName());
+        trimElement.addAttribute(new Attribute(XMLAttribute.PREFIX.getName(), "set"));
+        trimElement.addAttribute(new Attribute(XMLAttribute.SUFFIX_OVERRIDES.getName(), ","));
+        element.addElement(trimElement);
+
+        for (Column column : table.getColumns()) {
+            if (column.getConstraintType() == ColumnConstraintEnum.PRIMARY_KEY
+                    || column.getConstraintType() == ColumnConstraintEnum.UNION_PRIMARY_KEY) {
+                continue;
+            }
+            XmlElement ifElement = new XmlElement(XMLTag.IF.getName());
+            ifElement.addAttribute(new Attribute(XMLAttribute.TEST.getName(), StringUtil.removeUnderline(column.getName()) + " != null"));
+
+            StringBuilder setValueText = new StringBuilder();
+            setValueText.append(column.getName()).append(" = ");
+            setValueText.append("#{").append(StringUtil.removeUnderline(column.getName())).append(", jdbcType=")
+                    .append(JdbcTypeEnum.nameOf(column.getType().toUpperCase()).getName());
+            if (null != column.getTypeHandler()) {
+                setValueText.append(", typeHandler=").append(column.getTypeHandler());
+            }
+            setValueText.append("},");
+            TextElement setValue = new TextElement(setValueText.toString());
+            ifElement.addElement(setValue);
+
+            trimElement.addElement(ifElement);
+        }
+        return element;
+    }
+}
